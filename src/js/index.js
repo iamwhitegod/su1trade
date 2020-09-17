59f9bb69b6fd7a5da0164d102d43f074fce5ca00
whitegod <kmbagwu12@gmail.com>
2020-09-17 05:04:36 +0100

    Completed the header section. its fully responsive

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const mbMenu = document.querySelector(".mobile-nav-menu");
+mbMenu.addEventListener("click", (e) => {
+  mbMenu.classList.toggle("mobile-nav-menu__active");
+  document
+    .querySelector(".mobile-nav-menu__list")
+    .classList.toggle("menu__active");
+
+  // if(mbMenu.classList )
+  // // const mvlist = ;
+  // // console.log(mvlist);
+  // // mvlist.style.display = "block";
+});
+// mobile-nav-menu__active;
