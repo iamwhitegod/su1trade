695f66bd4fb40cebc54f93b768b6bfd9762955cf
whitegod <kmbagwu12@gmail.com>
2020-09-18 23:49:46 +0100

    Feat: added animation

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,46 @@ mbMenu.addEventListener("click", (e) => {
   // // mvlist.style.display = "block";
 });
 // mobile-nav-menu__active;
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNew = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNew) func.apply(context, args);
+  };
+}
+
+const slideCols = document.querySelectorAll(".slide-up");
+
+function checkSlide(e) {
+  slideCols.forEach((slideCol) => {
+    const elemLoc = window.scrollY + window.innerHeight;
+    const elemHeight = slideCol.clientHeight / 3;
+    const slideInAt = elemLoc - elemHeight;
+    //const colTop = slideCol.offsetTop + slideCol.height;
+
+    const colBottom = slideCol.offsetTop + slideCol.clientHeight;
+    const isHalfShown = slideInAt > slideCol.offsetTop;
+    const isNotScrolledPast = window.scrollY < colBottom;
+
+    // console.log(isHalfShown, isNotScrolledPast, slideInAt);
+
+    if (isHalfShown && isNotScrolledPast) {
+      slideCol.classList.add("active");
+    } else {
+      slideCol.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
