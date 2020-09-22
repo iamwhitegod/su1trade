f62e775c4e416e8901e36d105571f4dcfbb40b60
whitegod <kmbagwu12@gmail.com>
2020-09-23 00:31:48 +0100

    feat: Change animation timing and created a watch task for js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const { logError } = require("gulp-sass");
 const postcss = require("gulp-postcss");
+const concat = require("gulp-concat");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const browserSync = require("browser-sync").create();
@@ -15,12 +16,19 @@ gulp.task("sass", () => {
     .pipe(browserSync.stream());
 });
 
+gulp.task("js", () => {
+  return gulp.src("./src/js/**/*.js").pipe(gulp.dest("js"));
+});
+
 gulp.task("watch:sass", () => {
   browserSync.init({
     server: {
       baseDir: "./",
     },
   }),
-    gulp.watch("./src/sass/**/*.scss", gulp.series(["sass"])),
+    gulp.watch(
+      ["./src/sass/**/*.scss", "./src/js/**/*.js"],
+      gulp.series(["sass", "js"])
+    ),
     gulp.watch("./*.html").on("change", browserSync.reload);
 });
