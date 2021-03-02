aac23d28315d535bcb48b08b34d82c492e36f7e1
whitegod <kmbagwu12@gmail.com>
2021-03-02 22:19:20 +0100

    Started working on navigation

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,43 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+const htmlTemplateNames = [
+  "index",
+  "signup",
+  "signin",
+  "verify",
+  "about",
+  "ecommerce",
+  "email-ecommerce",
+  "email-Oauth",
+  "exchange",
+  "investment",
+  "license",
+  "news",
+  "partners",
+  "privacy",
+  "resetpassword",
+  "security",
+  "terms",
+  "tutorial",
+];
+const multipleHtmlTemplates = htmlTemplateNames.map(
+  (name) =>
+    new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: `./${name}.html`,
+      chunks: [`${name}`],
+    })
+);
+
 module.exports = {
   entry: ["babel-polyfill", "./src/js/index.js"],
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "js/bundle.js",
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: "./build",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -17,93 +46,8 @@ module.exports = {
       template: "./index.html",
     }),
 
-    new HtmlWebpackPlugin({
-      filename: "signup.html",
-      template: "./signup.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "signin.html",
-      template: "./signin.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "verify.html",
-      template: "./verify.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "resetpassword.html",
-      template: "./resetpassword.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "investment.html",
-      template: "./investment.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "about.html",
-      template: "./about.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "tutorial.html",
-      template: "./tutorial.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "ecommerce.html",
-      template: "./ecommerce.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "exchange.html",
-      template: "./exchange.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "partners.html",
-      template: "./partners.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "privacy.html",
-      template: "./privacy.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "license.html",
-      template: "./license.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "terms.html",
-      template: "./terms.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "news.html",
-      template: "./news.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "email-verify-email.html",
-      template: "./email-verify-email.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "email-ecommerce.html",
-      template: "./email-ecommerce.html",
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: "email-Oauth.html",
-      template: "./email-Oauth.html",
-    }),
-
     new CleanWebpackPlugin(),
-  ],
+  ].concat(multipleHtmlTemplates),
   module: {
     rules: [
       {
@@ -114,7 +58,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g|gif|mp4)$/i,
+        test: /\.(png|jpe?g|gif|svg|mp4)$/i,
         exclude: /node_modules/,
         use: {
           loader: "file-loader",
