6de6eede2bfa3f772c02c414195c312953c4900f
whitegod <kmbagwu12@gmail.com>
2021-03-04 07:15:58 +0100

    completed homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // import axios from "axios";
+import Typewriter from "typewriter-effect/dist/core";
 import * as summit from "./eventHandlers/summitForm";
 import * as e from "./eventHandlers/eventHandler";
 import * as canvas from "./eventHandlers/canvas";
@@ -8,6 +9,29 @@ import * as canvas from "./eventHandlers/canvas";
   e.setupEventListners();
   summit.collectRegistrationContact();
   summit.collectSignUpForm();
+
+  const heading = document.querySelector('[data--js="typewriter"]');
+
+  // Animations
+  const typewriter = new Typewriter(heading, {
+    loop: true,
+    delay: 75,
+  });
+
+  typewriter
+    .pauseFor(250)
+    .typeString(
+      `Invest, Buy and Sell Crptocurrencies, Learn Forex Trading and
+    E-commerce.`
+    )
+    .pauseFor(2500)
+    .deleteChars(71)
+    .typeString(` and earn 17% profit every month for 5months.`)
+    .pauseFor(2500)
+    .deleteChars(40)
+    .typeString(`get minimum of 28% or more monthly.`)
+    .pauseFor(25000)
+    .start();
 })();
 
 // canvas.init();
