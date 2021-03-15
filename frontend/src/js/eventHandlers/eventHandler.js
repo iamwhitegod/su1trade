2733a23caf56b3eadbd286ed4725ae3d19035256
whitegod <kmbagwu12@gmail.com>
2021-03-15 04:08:26 +0100

    added modal

--- frontend/src/js/eventHandlers/eventHandler.js
+++ frontend/src/js/eventHandlers/eventHandler.js
@@ -3,8 +3,12 @@ const menuPanel = document.querySelector('[data--js="menu--panel"]');
 const menuItems = Array.from(
   document.querySelectorAll('[data--js="menu--item"]')
 );
+const modalBtns = document.querySelectorAll('[data--js="open--modal"]');
 const menuLinks = document.querySelectorAll('[data--js="menu--link"]');
 const questionPanel = document.querySelectorAll(".FAQs__question");
+const modal = document.querySelector(".modal__container");
+const modalPopup = document.querySelector(".modal__popup");
+const modalOverlay = document.querySelector(".modal__overlay");
 
 // Setup eventlisteners
 export const setupEventListners = () => {
@@ -41,58 +45,61 @@ export const setupEventListners = () => {
       });
     });
   }
+
+  if (modal) {
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal.querySelector("img")) {
+        modal.classList.remove("open-modal");
+      }
+
+      if (e.target == modal.querySelector("button")) {
+        modal.classList.remove("open-modal");
+      }
+    });
+  }
+
+  if (modalPopup) {
+    modalPopup.addEventListener("click", (e) => {
+      if (e.target === modalPopup.querySelector("img")) {
+        modalPopup.classList.remove("active");
+        modalOverlay.classList.remove("active");
+      }
+
+      // if (e.target == modalPopup.querySelector("button")) {
+      //   modalPopup.classList.remove("active");
+      //   modalOverlay.classList.remove("active");
+      // }
+    });
+  }
+
+  if (modalBtns) {
+    console.log(modalBtns);
+    modalBtns.forEach((btn) => openModal(btn));
+  }
 };
 
 // setupEventListners();
-
-// const modal = document.querySelector(".modal__container");
-// const modalPopup = document.querySelector(".modal__popup");
-// const modalOverlay = document.querySelector(".modal__overlay");
 
 // /**********************************
 //  * modal
 //  **************** */
 
-// if (modal) {
-//   modal.addEventListener("click", (e) => {
-//     if (e.target === modal.querySelector("img")) {
-//       modal.classList.remove("open-modal");
-//     }
+export function openModal(trigger) {
+  trigger.addEventListener("click", () => {
+    if (modalPopup) {
+      modalPopup.classList.add("active");
+      modalOverlay.classList.add("active");
+      // console.log(modalOverlay);
+    }
+  });
+}
 
-//     if (e.target == modal.querySelector("button")) {
-//       modal.classList.remove("open-modal");
-//     }
-//   });
-// }
-
-// if (modalPopup) {
-//   modalPopup.addEventListener("click", (e) => {
-//     if (e.target === modalPopup.querySelector("img")) {
-//       modalPopup.classList.remove("active");
-//       modalOverlay.classList.remove("active");
-//     }
-
-//     if (e.target == modalPopup.querySelector("button")) {
-//       modalPopup.classList.remove("active");
-//       modalOverlay.classList.remove("active");
-//     }
-//   });
-// }
-
-// export function openModal(trigger) {
-//   trigger.addEventListener("click", () => {
-//     if (modal) {
-//       modal.classList.add("open-modal");
-//     }
-//   });
-// }
-
-// export function openModalPopup() {
-//   if (modalPopup) {
-//     modalPopup.classList.add("active");
-//     modalOverlay.classList.add("active");
-//     console.log(modalOverlay);
-//   }
-// }
+export function openModalPopup() {
+  if (modalPopup) {
+    modalPopup.classList.add("active");
+    modalOverlay.classList.add("active");
+    console.log(modalOverlay);
+  }
+}
 
 // openModalPopup();
